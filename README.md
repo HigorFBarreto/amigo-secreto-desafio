b15b06b9c1e2ec2c41f124ee34b6a28d5c05bf9c
Higor Barreto <higorbarreto.hf@gmail.com>
2025-02-03 23:53:52 -0300

    feat: desafio completo

---
+++ README.md
@@ -0,0 +1,33 @@
+# Projeto Amigo Secreto
+
+Este projeto foi desenvolvido como parte do curso Oracle One.
+
+## Funcionalidades
+
+- Cadastro de participantes
+- Sorteio automático
+
+## Tecnologias Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Como Executar o Projeto
+
+1. Clone o repositório;
+
+2. Navegue até o diretório do projeto:
+    ```bash
+    cd challenge-amigo-secreto-desafio
+    ```
+3. Abra o arquivo `index.html` no seu navegador.
+
+## Contribuição
+
+Contribuições fechada, pois é um desafio realizado no curso Oracle One
+
+
+## Contato
+
+Para mais informações, entre em contato comigo pelo e-mail: higorbarreto.hf@gmail.com
--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+//  Função para adicionar amigo
+
+function adicionarAmigo(novoAmigo) {
+  // Valida se o novoAmigo é válido ( não é vazio ou uma string que não seja um nome )
+  console.log("Adicionando amigo", novoAmigo);
+  if (!novoAmigo || novoAmigo.length < 4) {
+    alert("Por favor insira um nome válido.");
+
+    return;
+  }
+
+  amigos.push(novoAmigo);
+
+  const inputDoNovoAmigo = document.getElementById("amigo");
+  inputDoNovoAmigo.value = "";
+
+  listaAmigos();
+
+  console.log("Nova lista de amigos", amigos);
+}
+
+function listaAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+
+    // Limpa a lista de amigos
+    listaAmigos.innerHTML = "";
+
+    // Adiciona cada amigo na lista de amigos usando for
+    for (let posicao = 0; posicao < amigos.length; posicao++) {
+        listaAmigos.innerHTML += `<li>${amigos[posicao]}</li>`;
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length < 1) {
+      alert(" Adicione amigos antes de sortear.");
+  
+      return;
+    }
+  
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+  
+    document.getElementById("resultado").innerHTML =
+        "Seu amigo secreto é : " + amigoSorteado;
+}
